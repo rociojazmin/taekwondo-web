b2ec7cf79598181a7cb18730fe0e89da98d6841c
rociojazmin <rociomiramont@gmail.com>
2022-01-30 23:12:24 -0300

    - Cambie el forEach por un for - Ejemplo de un modal (aun no anda) - Ejemplo de juguete de un switch

--- assets/js/logica.js
+++ assets/js/logica.js
@@ -12,13 +12,44 @@ var carousel = new bootstrap.Carousel(mycarousel, {
 // Animacion Fade-in
 
 const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
+    for (let i = 0; i < entries.length; i++) {
+        const entry = entries[i];
         if(entry.isIntersecting){
             document.querySelectorAll(".animado")[0].classList.add("fadeinleft");
             document.querySelectorAll(".animado")[1].classList.add("fadeintop");
             document.querySelectorAll(".animado")[2].classList.add("fadeinright");
         }
-    })
+    }
 });
 
 observer.observe(document.querySelector(".contenedor-animacion"));
+
+
+// Modal
+
+// setTimeout(() => {
+//     var myModal = new bootstrap.Modal(document.getElementById('myModal'), options);
+//     myModal.show();
+// }, 5000);
+
+
+// Switch
+
+var nombre = "Calaiego";
+var genero = "h";//"m", "otro"
+
+var prefijo;
+switch (genero) {
+    case "h":
+        prefijo = "Señor";
+        break;
+    case "m":
+        prefijo = "Señora";
+        break;
+    default:
+        prefijo = "Hola";
+        break;
+}
+
+var mensaje = prefijo +  " " + nombre + ", ya enviamos tu solicitud...";
+console.log(mensaje);
